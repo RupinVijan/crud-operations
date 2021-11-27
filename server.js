fc412845d3b13b7c75ae3b9a1d37f59dabafdb81
RupinVijan <rupinvijan@gmail.com>
2021-11-27 16:50:07 +0530

    final commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose')
-mongoose.connect('mongodb://localhost:27017/Node-test');
+mongoose.connect('');
 var db=mongoose.connection;
 
 db.on('error',console.error.bind(console,"Connection error : "))
